29ed6da845649e26f6550322a3cee03cf1182d1a
elmallaheg <elmallah.eg@gmail.com>
2026-07-06 13:29:27 +0300

    Update config.js

--- complaints-app 2/complaints-spa/config.js
+++ complaints-app 2/complaints-spa/config.js
@@ -6,5 +6,5 @@
 
 const CONFIG = {
   SUPABASE_URL: "https://fhkwziglmyviuarwmlym.supabase.co",
-  SUPABASE_ANON_KEY: "ضع_هنا_مفتاح_anon_العام"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoa3d6aWdsbXl2aXVhcndtbHltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMDcyNTAsImV4cCI6MjA5ODg4MzI1MH0.cPuHGPZqs1X6odpj0xYTxUCgUTsXetacp9yMnVAGbMM"
 };
